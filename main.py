8826dcd0c936a6cebc3c473ce3a890771973497a
ClydeUrov <clydeurov@gmail.com>
2023-06-19 10:19:40 +0300

    Part 19 -> Handling Errors

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # from enum import Enum
 # from typing import Optional
 import re
-from pydantic import BaseModel, Field, HttpUrl
+from datetime import datetime, time, timedelta
+from typing import Literal, Union
+from uuid import UUID
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
 
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, Cookie, \
+    Header, status, Form, File, UploadFile, HTTPException, Request
+
+from fastapi.responses import HTMLResponse, JSONResponse
 
 app = FastAPI()
 
@@ -260,48 +266,343 @@ app = FastAPI()
 #     return blahs
 
 # Part 10 -> Declare Request Example Data
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#
+#     # class Config:
+#     #     schema_extra = {
+#     #         "example": {
+#     #             "name": "Foo",
+#     #             "description": "A very nice item",
+#     #             "price": 16.25,
+#     #             "tax": 1.67,
+#     #         }
+#     #     }
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(
+#     ...,
+#     examples={
+#         "normal": {
+#             "summary": "A normal example",
+#             "description": "A __normal__ python works _correctly_",
+#             "value": {"name": "Foo",
+#             "description": "A very nice Item",
+#             "price": 16.25,
+#             "tax": 1.67}
+#         },
+#         "converted": {
+#             "summary": "An example with converted",
+#             "description": "FastApi can convert price",
+#             "value": {"name": "Bar", "price": "16.25"}
+#         },
+#         "invalid": {
+#             "summary": "Invalid data is rejected with an error",
+#             "description": "Hello Youtubers",
+#             "value": {
+#                 "name": "Baz", "price": "sixteen point two five"
+#             }
+#         }
+#     }
+# )):
+#     results = {"item_id": item_id, "item": item}
+#     return results
 
-    # class Config:
-    #     schema_extra = {
-    #         "example": {
-    #             "name": "Foo",
-    #             "description": "A very nice item",
-    #             "price": 16.25,
-    #             "tax": 1.67,
-    #         }
-    #     }
+# Part 11 Extra Date Types
+# @app.put("/items/{item_id}")
+# async def read_items(
+#         item_id: UUID,
+#         start_date: datetime | None = Body(None),
+#         end_date: datetime | None = Body(None),
+#         repeat_at: time | None = Body(None),
+#         process_after: timedelta | None = Body(None),
+# ):
+#     start_process = start_date + process_after
+#     duration = end_date - start_process
+#     return {"item_id": item_id, "start_date": start_date, "end_date": end_date,
+#             "repeat_at": repeat_at, "process_after": process_after,
+#             "start_process": start_process, "duration": duration}
+
+# Part 12 -> Cookie and Header Parameters
+# @app.get("/items")
+# async def read_items(
+#     cookie_id: str | None = Cookie(None),
+#     accept_encoding: str | None = Header(None),
+#     sec_ch_ua: str | None = Header(None),
+#     user_agent: str | None = Header(None),
+#     x_token: list[str] | None = Header(None),
+# ):
+#     return {
+#         "cookie_id": cookie_id,
+#         "Accept-Encoding": accept_encoding,
+#         "sec-ch-ua": sec_ch_ua,
+#         "User-Agent": user_agent,
+#         "X-Token values": x_token
+#     }
+
+# Part 13 -> Response Model
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+#     tags: list[str] = []
+#
+#
+# items = {
+#     'foo': {'name': 'Foo', 'price': 50.2},
+#     'bar': {'name': 'Bar', 'description': 'The bartenders', 'price': 62, 'tax': 20.2},
+#     'baz': {'name': 'Bar', 'description': None, 'price': 50.2, 'tax': 10.5, 'tags': []}
+# }
+#
+#
+# @app.get('/items/{item_id}', response_model=Item, response_model_exclude_unset=True)
+# async def read_item(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+#
+#
+# @app.post("/items/", response_model=Item)
+# async def create_item(item: Item):
+#     return item
+#
+#
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserIn(UserBase):
+#     password: str
+#
+#
+# class UserOut(UserBase):
+#     pass
+#
+#
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+#
+#
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={'name', 'description'}
+# )
+# async def read_item_name(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+#
+#
+# @app.get(
+#     "/items/{item_id}/public",
+#     response_model=Item,
+#     response_model_exclude={'tax'}
+# )
+# async def read_items_public_data(item_id: Literal['foo', 'bar', 'baz']):
+#     return items[item_id]
+
+# Part 14 -> Extra Models
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserIn(UserBase):
+#     password: str
+#
+#
+# class UserOut(UserBase):
+#     pass
+#
+#
+# class UserInDB(UserBase):
+#     hashed_password: str
+#
+#
+# def fake_password_hasher(raw_password: str):
+#     return f'supersecret{raw_password}'
+#
+#
+# def fake_save_user(user_in: UserIn):
+#     hashed_password = fake_password_hasher(user_in.password)
+#     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+#     print("userin.dict", user_in.dict())
+#     # print(
+#     #     UserInDB(
+#     #         username='hello',
+#     #         hashed_password='password',
+#     #         email='hello@gmail.com',
+#     #         hello='world',
+#     #         foo='bar'
+#     #     )
+#     # )
+#     # const userINDb = {
+#     #     ...user_in,
+#     #     hashed_password=hashed_password
+#     # }
+#     print("User 'saved'.")
+#     return user_in_db
+#
+#
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user_in: UserIn):
+#     user_saved = fake_save_user(user_in)
+#     return user_saved
+#
+#
+# class BaseItem(BaseModel):
+#     description: str
+#     type: str
+#
+#
+# class CarItem(BaseItem):
+#     type = 'car'
+#
+#
+# class PlaneItem(BaseItem):
+#     type = 'plane'
+#     size: int
+#
+#
+# items = {
+#     'item1': {'description': "All my friends ride a low ride", 'type': 'car'},
+#     'item2': {'description': "Music ia my aeroplane, it's  me aeroplane", 'type': "plane", "size": 5},
+# }
+#
+#
+# @app.get("/item/{item_id}", response_model=PlaneItem | CarItem)
+# async def read_item(item_id: Literal["item1", "item2"]) -> PlaneItem | CarItem:
+#     return items[item_id]
+#
+#
+# class ListItem(BaseModel):
+#     name: str
+#     description: str
+#
+#
+# list_items = [
+#     {'name': "Foo", 'description': "There comes my hero"},
+#     {'name': "Red", 'description': "It's my aeroplane"}
+# ]
+#
+#
+# @app.get("/list_items/", response_model=list[ListItem])
+# async def read_items():
+#     return items
+#
+#
+# @app.get('/arbitrary', response_model=dict[str, float])
+# async def get_arbitrary():
+#     return {"foo": 1, "bar": '2'}
+
+# Part 15 -> Response Status Code
+# @app.post('/items/', status_code=status.HTTP_201_CREATED)
+# async def create_item(name: str):
+#     return {'name': name}
+#
+#
+# @app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_item(pk: str):
+#     print('pk', pk)
+#     return pk
+#
+#
+# @app.get("/items/", status_code=status.HTTP_302_FOUND)
+# async def read_items_redirect():
+#     return {'hello': 'world'}
+
+# Part 16 -> Form Fields
+# class User(BaseModel):
+#     username: str
+#     password: str
+#
+#
+# @app.post("/login/")
+# async def login(username: str = Form(...), password: str = Form(...)):
+#     print('password', password)
+#     return {'username': username}
+#
+#
+# @app.post('/login-json')
+# async def login_json(username: str = Body(...), password: str = Body(...)):
+#     print('password', password)
+#     return {'username': username}
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(
-    ...,
-    examples={
-        "normal": {
-            "summary": "A normal example",
-            "description": "A __normal__ python works _correctly_",
-            "value": {"name": "Foo",
-            "description": "A very nice Item",
-            "price": 16.25,
-            "tax": 1.67}
-        },
-        "converted": {
-            "summary": "An example with converted",
-            "description": "FastApi can convert price",
-            "value": {"name": "Bar", "price": "16.25"}
-        },
-        "invalid": {
-            "summary": "Invalid data is rejected with an error",
-            "description": "Hello Youtubers",
-            "value": {
-                "name": "Baz", "price": "sixteen point two five"
-            }
-        }
-    }
-)):
-    results = {"item_id": item_id, "item": item}
-    return results
+# Part 17 -> Request Files, Uploads Files
+# @app.post("/files/")
+# async def create_file(files: list[bytes] = File(..., description='A file read as bytes')):
+#     return {'file': [len(file) for file in files]}
+#
+#
+# @app.post("/upload_file")
+# async def create_upload_file(
+#         files: list[UploadFile] = File(..., description='A file read in UploadFile')
+# ):
+#     if not files:
+#         return {"message": 'No upload file sent'}
+#     content = [await file.read() for file in files]
+#     return {"filename": [file.filename for file in files], "content": content}
+#
+#
+# @app.get('/')
+# async def main():
+#     content = """
+#     <body>
+#         <form action="/files/" enctype="multipart/form-data" method="post">
+#             <input name="files" type="file" multiple>
+#             <input type="submit">
+#         </form>
+#         <form action="/upload_file/" enctype="multipart/form-data" method="post">
+#             <input name="files" type="file" multiple>
+#             <input type="submit">
+#         </form>
+#     </body>
+#     """
+#     return HTMLResponse(content=content)
+
+# Part 18 -> Request Form and Files
+# @app.post("/files/")
+# async def create_file(
+#         file: bytes = File(...), fileb: UploadFile = File(...), hello: str = Body(...),
+#         token: str = Form(...)
+# ):
+#     return {
+#         "file_size": len(file),
+#         "token": token,
+#         "fileb_content_type": fileb.content_type,
+#         "hello": hello
+#     }
+
+# Part 19 -> Handling Errors
+items = {"foo": "The foo Wrestlers"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"}
+        )
+    return {"item": items[item_id]}
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(status_code=418, content={'message': f'Oops! {exc.name} did somthing. There gose a rainbow...'})
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == 'yolo':
+        raise UnicornException(name=name)
+    return {'unicorne_name': name}
