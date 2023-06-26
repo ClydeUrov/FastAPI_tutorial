c4ed0b7e1ef5e50f3155e9ea4a6976b22c28a4d1
ClydeUrov <clydeurov@gmail.com>
2023-06-26 13:10:38 +0300

    Part 30 -> Bigger Application - Multiple Files

--- main.py
+++ main.py
@@ -1075,23 +1075,24 @@ app = FastAPI()
 #     return [{'item_id': 'Foo', "owner": current_user.username}]
 
 # Part 28 - Middleware and CORS
-class MyMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        start_time = time.time()
-        response = await call_next(request)
-        process_time = time.time() - start_time
-        response.headers['X-Process-Time'] = str(process_time)
-        return response
+# class MyMiddleware(BaseHTTPMiddleware):
+#     async def dispatch(self, request: Request, call_next):
+#         start_time = time.time()
+#         response = await call_next(request)
+#         process_time = time.time() - start_time
+#         response.headers['X-Process-Time'] = str(process_time)
+#         return response
+#
+#
+# origins = ["http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:3000",
+#            "http://localhost:5173", "http://localhost:5173/"]
+# app.add_middleware(MyMiddleware)
+# app.add_middleware(CORSMiddleware, allow_origins=origins)
+#
+#
+# @app.get('/blah')
+# async def blah():
+#     return {"hello": "world"}
 
-
-origins = ["http://localhost:8000", "http://127.0.0.1:8000", "http://localhost:3000",
-           "http://localhost:5173", "http://localhost:5173/"]
-app.add_middleware(MyMiddleware)
-app.add_middleware(CORSMiddleware, allow_origins=origins)
-
-
-@app.get('/blah')
-async def blah():
-    return {"hello": "world"}
-
-#Part 29 -> SQL Relational Databases
+# Part 29 -> SQL Relational Databases
+# Part 30 -> Bigger Application - Multiple Files
